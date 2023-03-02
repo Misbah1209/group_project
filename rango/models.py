817fa9d3a4ec7375ee8b855ffd51e4d29a989e77
misbah shaikh <2796456S@student.gla.ac.uk>
2023-03-02 18:01:28 +0000

    model

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    img = models.CharField(max_length=128)
+    price = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
